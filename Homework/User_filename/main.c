2c2421d126059617c5bde7d8677ab7cb52d21d42
Ioann Goremykin <tanknaydanova@yandex.ru>
2017-12-09 12:56:10 +0300

    Все изменения после пары 9.12.2017. Дополнена программа User_filename

--- Homework/User_filename/main.c
+++ Homework/User_filename/main.c
@@ -1,16 +1,44 @@
 
-#include<stdio.h>
+#include<stdio.h> // для scanf, printf, fgets
+#include<string.h> // для операций со строками: str~
 
-char s[10]={22};
+//char s[10]={22};
+char fname[10] = {0};
 
 int main(void)
 {
 	printf("Начало\n");
-	for(int n = 1; n<=10; n++)
+	
+	/*for(int n = 1; n<=10; n++)
 	{
 		printf("%d",s[n]);
+	}*/
+	
+	for(int n = 0; n<=9; n++)
+	{
+		printf("%d",fname[n]);
 	}
-	printf("END\nКонец\n");
+	printf("\n");
+	
+	fgets(fname,sizeof(fname),stdin); // strlen или sizeof
+	//scanf("%s",fname);
+	fname[strlen(fname)-1] = 0;
+	
+	printf("%s\n",fname);
+	printf("END\n%lu | %d | %c | %d | %d\n",
+		   sizeof(fname),
+		   fname[sizeof(fname)-1],
+		   fname[strlen(fname)-1],
+		   (int)strlen(fname),
+		   fname[6-1]);
+	
+	
+	for(int n = 0; n<=9; n++)
+	{
+		printf("%d ",fname[n]);
+	}
+	printf("\n");
+	printf("Конец\n");
 }
 
 
