0ff1695f197ed5e475715e1ce473d7301ef32b28
Ioann Goremykin <tanknaydanova@yandex.ru>
2017-12-16 14:42:14 +0300

    Результаты 16.12.217сб - Полностью дописана программа User_filename В Задаче 2.2: - Доступно произвольное имя файла - Создаётся и заполняется массив - есть ошибки ...

--- Homework/2/2/main.c
+++ Homework/2/2/main.c
@@ -1,5 +1,7 @@
 
-#include<stdio.h> // printf и scanf
+#include<stdio.h> // для scanf, printf, fgets
+#include<string.h> // для операций со строками: str~
+#include<stdlib.h> // для динамических массивов: malloc, free
 
 
 /*int Rearrange(..., L);
@@ -17,14 +19,15 @@ int main(void)
 	 *  Переставляет элементы массива в обратном порядке
 	 *  Ввод ограничен форматом, точнсть не ограничена
 	 *  Точность вывода 16 цифр после запятой
+	 *  Ввод рабочих данных без ошибок; проверки вида нет
 	 *  Наибольшая длинна имени файла расчитана на
 	 *  Windows API, файловую систему NTFS - 260 символов
 	 */
-	unsigned int Q;
+	unsigned int Q, Len;
 	FILE *pfIN, *pfOUT;
-	char FName[260] = {0};
-	Q = 0;
-	
+	char fName[260];
+	int *Numbers;
+	Q = Len = 0;
 	printf("-------------main.c-------------\n");
 	printf("- - - - - Задача 2.2 - - - - -\n");
 	printf("Переставляет элементы массива в обратном порядке\n");
@@ -39,21 +42,31 @@ int main(void)
 			pfIN = fopen("INPUT.txt","r");
 	    } else if(Q==1) // Альтер. имя файла
 			{
+				// Пояснения см. в User_filename
 				printf("Напишите его адрес-имя в текущей библиотеке\n");
 				printf("Ограничение 259 символов\n");
-				fgets(FName, sizeof(FName)-1, stdin);
-				for(unsigned int n = 0; n < sizeof(FName); n++)
+				fgets(fName,2,stdin);
+				fgets(fName, sizeof(fName), stdin);
+				printf("Считана строка: %s\n",fName);
+				if(strlen(fName)+1 == sizeof(fName))
 				{
-					printf("%d", FName[n]);
+					fName[strlen(fName)] = 0;
+				} else {
+					fName[strlen(fName)-1] = 0;
 				}
-				printf("END\n");
-				// scanf(...)
-				//pfIN = fopen("...","r");
+				printf("Считан путь: %s\n", fName);
+				pfIN = fopen(fName,"r");
 			} else {
 				return -1; // Ответ на вопр-1ос не 1 и не 0
 			}
 	    if(pfIN != NULL) // Основной ход программы
 	    {
+			fscanf(pfIN,"%ud",&Len);
+			Numbers = malloc(Len*sizeof(int));
+			for(unsigned int N=0; N<sizeof(Numbers); N++)
+			{
+				fscanf(pfIN,"%d",&Numbers[N]);
+			}
 			/*if (Calculate(pfIN,&Middle)<0) // Запуск функции
 			{
 				return -1; // Ошибка в функции
@@ -74,13 +87,14 @@ int main(void)
 				} else if(Q==1) // На экран
 					{
 						//printf("Такое среднее равно %.16lf\n", Middle);
-						return 0;
+						//return 0;
 					} else {
 						return -1; // Ответ на II вопрос не 1 и не 0
 					}
 			} else {
 				return -1; // Q не считано (2й вопрос)
 			}
+			free(Numbers);
 	    } else {
 			printf("Файл не удаётся открыть\n");
 			return -1; // Файл не открывается
--- Homework/User_filename/main.c
+++ Homework/User_filename/main.c
@@ -2,40 +2,45 @@
 #include<stdio.h> // для scanf, printf, fgets
 #include<string.h> // для операций со строками: str~
 
-//char s[10]={22};
-char fname[10] = {0};
+char fname[10]; // = {54}; - инициализирует только 1ый символ
 
 int main(void)
 {
 	printf("Начало\n");
 	
-	/*for(int n = 1; n<=10; n++)
-	{
-		printf("%d",s[n]);
-	}*/
-	
 	for(int n = 0; n<=9; n++)
 	{
-		printf("%d",fname[n]);
+		printf("%d (%c) ",fname[n],fname[n]);
 	}
-	printf("\n");
+	printf("\n**********\n");
 	
-	fgets(fname,sizeof(fname),stdin); // strlen или sizeof
+	//fgets(fname,2,stdin); - если перед fgets стоит scanf
+	//scanf оставляет после себя в буффере перенос строки
+	fgets(fname,sizeof(fname),stdin); // strlen или sizeof. -1 включён
 	//scanf("%s",fname);
-	fname[strlen(fname)-1] = 0;
-	
-	printf("%s\n",fname);
-	printf("END\n%lu | %d | %c | %d | %d\n",
-		   sizeof(fname),
-		   fname[sizeof(fname)-1],
-		   fname[strlen(fname)-1],
-		   (int)strlen(fname),
-		   fname[6-1]);
-	
+	printf("Считана строка: %s\n",fname);
+	printf("sizeof строки: %lu\n", sizeof(fname));
+	printf("strlen строки: %d\n", (int)strlen(fname));
+	if(strlen(fname)+1 == sizeof(fname))
+	// Если слово max длины и перенос строки [10] не появится. Из-за fgets >= не нужно.
+	{
+		fname[strlen(fname)] = 0;
+		// Не залезет на следующий массив. Бесполезно, если нам выдали чистую память
+		// (массив из 0), но это может быть не так
+	} else {
+		fname[strlen(fname)-1] = 0;
+		// Отбрасывает перенос строки, вводимый Enter'ом
+	}
+	printf("Обработана строка: %s\n",fname);
+	printf("sizeof строки: %lu\n", sizeof(fname));
+	printf("strlen строки: %d\n", (int)strlen(fname));
+	printf("последний символ строки (sizeof-1): %d (%c)\n", fname[sizeof(fname)-1], fname[sizeof(fname)-1]);
+	printf("последний символ строки (strlen-1): %d (%c)\n", fname[strlen(fname)-1], fname[strlen(fname)-1]);
+	printf("6й символ строки: %d (%c)\n", fname[6-1], fname[6-1]);
 	
 	for(int n = 0; n<=9; n++)
 	{
-		printf("%d ",fname[n]);
+		printf("%d (%c) ",fname[n],fname[n]);
 	}
 	printf("\n");
 	printf("Конец\n");
